9653c82a40a2c49b172e47753e5a41f656bfbb49
CCCodes <caitlinchou@gmail.com>
2020-12-17 22:48:18 -0500

    day 1

---
+++ day1.py
@@ -0,0 +1,29 @@
+def part1(inp_list):
+    for num in inp_list:
+        if num == 1010:
+            print("1010, double check")
+        if 2020 - num in inp_list:
+            return num * (2020 - num)
+
+
+def part2(inp_list):
+    for i, num1 in enumerate(inp_list):
+        for j, num2 in enumerate(inp_list):
+            if j == i:
+                continue
+            num3 = 2020 - num1 - num2
+            if num3 in inp_list:
+                if num3 == num1 and num3 == num2:
+                    if inp_list.count(num3) < 3:
+                        continue
+                elif num3 == num1 or num3 == num2:
+                    if inp_list.count(num3) == 1:
+                        continue
+                return num3 * num1 * num2
+
+
+if __name__ == "__main__":
+    inp_f = open("day1_1.txt", "r")
+    inp = inp_f.read()
+    inp_list = [int(num_str) for num_str in inp.split("\n") if num_str != ""]
+    print(part2(inp_list))
